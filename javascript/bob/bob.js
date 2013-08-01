75489ff91fde8e6b346ba3f329da63e69ddd2fe4
Dennis Riis <dr@driis.dk>
2013-08-01 15:06:13 +0200

    Bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,3 +1,16 @@
 Bob = function() {
-	hey: function() { return "hey"; }
+	var responses = {
+		"WATCH OUT!": "Woah, chill out!",
+		"Let's go make out behind the gym!": "Whatever.",
+		"1, 2, 3 GO!": "Woah, chill out!",
+		"ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!": "Woah, chill out!",
+		"Does this cryogenic chamber make me look fat?" : "Sure.",
+		'': "Fine, be that way!"
+	}
+	return {
+		hey: function(phrase) {
+			var response = responses[phrase];			
+			return response || "Whatever.";
+		}
+	}
 }
--- javascript/bob/bob_test.spec.js
+++ javascript/bob/bob_test.spec.js
@@ -8,32 +8,32 @@ describe("Bob", function() {
     expect(result).toEqual('Whatever.');
   });
 
-  xit("shouting", function() {
+  it("shouting", function() {
     var result = bob.hey('WATCH OUT!');
     expect(result).toEqual('Woah, chill out!');
   });
 
-  xit("asking a question", function() {
+  it("asking a question", function() {
     var result = bob.hey('Does this cryogenic chamber make me look fat?');
     expect(result).toEqual('Sure.');
   });
 
-  xit("talking forcefully", function() {
+  it("talking forcefully", function() {
     var result = bob.hey("Let's go make out behind the gym!");
     expect(result).toEqual('Whatever.');
   });
 
-  xit("shouting numbers", function() {
+  it("shouting numbers", function() {
     var result = bob.hey('1, 2, 3 GO!');
     expect(result).toEqual('Woah, chill out!');
   });
 
-  xit("shouting with special characters", function() {
+  it("shouting with special characters", function() {
     var result = bob.hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!');
     expect(result).toEqual('Woah, chill out!');
   });
 
-  xit("silence", function() {
+  it("silence", function() {
     var result = bob.hey('');
     expect(result).toEqual('Fine, be that way!');
   });
