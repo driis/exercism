299732727c1bf59748a7d270d16595c687d492c0
Dennis Riis <driis@ebay.com>
2013-08-03 18:34:02 +0200

    First working implementation of bob in clojure.

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,5 +1,15 @@
-(ns bob)
+(ns bob (:use clojure.string))
 
-(defn respond-for 
-	([x] (str "Whatever."))
+(defn response-for [x] 
+	(if (blank? x) 
+		"Fine, be that way."
+		(if (= (upper-case x) x)
+			"Woah, chill out!"
+			(if (= \? (.charAt x (- (.length x) 1)))
+				"Sure."
+				"Whatever."
+			)
+		)
+	)
+
 )
