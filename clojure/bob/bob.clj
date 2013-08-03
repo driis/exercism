293d48d68420b76de2d1f84aeee3028fe99c90b6
Dennis Riis <driis@ebay.com>
2013-08-03 18:42:35 +0200

    Refactor to use cond

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,15 +1,10 @@
 (ns bob (:use clojure.string))
 
-(defn response-for [x] 
-	(if (blank? x) 
-		"Fine, be that way."
-		(if (= (upper-case x) x)
-			"Woah, chill out!"
-			(if (= \? (.charAt x (- (.length x) 1)))
-				"Sure."
-				"Whatever."
-			)
-		)
-	)
-
+(defn response-for [x]
+	(cond 
+		(blank? x) "Fine, be that way."
+		(= (upper-case x) x) "Woah, chill out!"
+		(= \? (.charAt x (- (.length x) 1))) "Sure."
+		true "Whatever."
+	) 	
 )
