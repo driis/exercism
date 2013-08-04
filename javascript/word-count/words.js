9a1bb65e7c3eddbcd4b40be630f0ae7867b689fe
Dennis Riis <driis@ebay.com>
2013-08-04 20:21:28 +0200

    Move declaration of idx

--- javascript/word-count/words.js
+++ javascript/word-count/words.js
@@ -1,10 +1,9 @@
 function Words(sentence){
     "use strict";
-    var idx;
     sentence = sentence.toLowerCase();
     var wordMatches = sentence.match(/\w+/g);
     var result = {};
-    for(idx = 0 ; idx < wordMatches.length ; idx++) {
+    for(var idx = 0 ; idx < wordMatches.length ; idx++) {
         result[wordMatches[idx]] = (result[wordMatches[idx]] || 0) + 1;
     }
     return { count: result };
