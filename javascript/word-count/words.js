d9297a0dfbb6825e0a4d7edb0ca31bbd52a88ca9
Dennis Riis <driis@ebay.com>
2013-08-04 20:20:41 +0200

    First working words solution

--- javascript/word-count/words.js
+++ javascript/word-count/words.js
@@ -1,4 +1,13 @@
-Words = function() {
+function Words(sentence){
     "use strict";
-      
+    var idx;
+    sentence = sentence.toLowerCase();
+    var wordMatches = sentence.match(/\w+/g);
+    var result = {};
+    for(idx = 0 ; idx < wordMatches.length ; idx++) {
+        result[wordMatches[idx]] = (result[wordMatches[idx]] || 0) + 1;
+    }
+    return { count: result };
 };
+
+module.exports = Words;
