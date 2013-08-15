eed9e520f02ca0a9b42a8ace3b3eb2b90ad3f4b1
Dennis Riis <driis@ebay.com>
2013-08-15 10:43:58 +0200

    Use a separate counting function.

--- javascript/word-count/words.js
+++ javascript/word-count/words.js
@@ -1,12 +1,17 @@
 function Words(sentence){
     "use strict";
-    sentence = sentence.toLowerCase();
-    var wordMatches = sentence.match(/\w+/g);
-    var result = {};
-    for(var idx = 0 ; idx < wordMatches.length ; idx++) {
-        result[wordMatches[idx]] = (result[wordMatches[idx]] || 0) + 1;
+    
+    return { count: countWords(sentence) };
+
+    function countWords(sentence){
+        sentence = sentence.toLowerCase();
+        var wordMatches = sentence.match(/\w+/g);
+        var result = {};
+        for(var idx = 0 ; idx < wordMatches.length ; idx++) {
+            result[wordMatches[idx]] = (result[wordMatches[idx]] || 0) + 1;
+        }    
+        return result;
     }
-    return { count: result };
 };
 
 module.exports = Words;
