70e78a7ca045bd566812ee183ae3ae03a465778a
Dennis Riis <driis@ebay.com>
2013-08-16 21:20:30 +0200

    Anagram solution based 'normalized' string version of the words - (set based sln)

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,22 +1,14 @@
 function Anagram(word) {
+    "use strict";
+    
+    var normalizedWord = normalize(word);
 
     function isAnagram(candidate) {
-        if (word.length !== candidate.length)
-            return false;
+        return normalize(candidate) === normalizedWord;
+    }
 
-        for(var i = 0 ; i < word.length ; i++) {
-            var charPos = candidate.indexOf(word.charAt(i));
-            if (charPos === -1)
-                return false;
-
-            candidate = removeCharAt(candidate, charPos);
-        }
-
-        return true;
-    };
-
-    function removeCharAt(str, index) {
-        return str.slice(0,index) + str.slice(index + 1);
+    function normalize(str) {
+        return str.split("").sort().join("");
     }
 
     return {
