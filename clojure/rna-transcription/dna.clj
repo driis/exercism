5a862239afa04351d0e5bb4155d55b23be266c0a
Dennis Riis <driis@ebay.com>
2013-08-17 11:14:33 +0200

    Use symbols for Thymine and Uracil

--- clojure/rna-transcription/dna.clj
+++ clojure/rna-transcription/dna.clj
@@ -1,7 +1,12 @@
 (ns dna)
 
+(def Thymine \T)
+(def Uracil  \U)
+
 (defn- strand-convert [ch]
-    (if (= \T ch) \U ch)
+    (if (= Thymine ch) 
+        Uracil 
+        ch)
 )
 
 (defn to-rna [dna]
