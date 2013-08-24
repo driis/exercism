1fac185e1d07aca80fb462271c901ff1104ea91b
Dennis Riis <driis@ebay.com>
2013-08-24 20:11:15 +0200

    Using let, better names, easier to read strings

--- clojure/beer-song/beer.clj
+++ clojure/beer-song/beer.clj
@@ -7,26 +7,22 @@
     (= count 1)   "1 bottle"
     :else         (str count " bottles")))
 
-(defn- beer-string [count]
+(defn- singular-string [count]
   (if (= count 1) 
     "it" 
     "one"))
 
 (defn- first-strophe [count]
   (string/capitalize 
-    (str (count-string count)
-      " of beer on the wall, " 
-      (count-string count) 
-      " of beer.")))
+    (let [n-bottles (count-string count)]
+      (str n-bottles " of beer on the wall, " n-bottles " of beer."))))
 
 (defn- second-strophe [count]
   (if (zero? count)
     "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    (str "Take "
-      (beer-string count)
-      " down and pass it around, "
-      (count-string (- count 1))
-      " of beer on the wall.\n")))
+    (let [singular (singular-string count) 
+          n-bottles (count-string (dec count))]
+      (str "Take " singular " down and pass it around, " n-bottles " of beer on the wall.\n"))))
   
 (defn verse [count]
   (str (first-strophe count) "\n" (second-strophe count)))
@@ -34,4 +30,4 @@
 (defn sing 
   ([begin] (sing begin 0))
   ([begin end]
-    (str (string/join "\n" (map verse (range begin (- end 1) -1))) "\n")))
+    (str (string/join "\n" (map verse (range begin (dec end) -1))) "\n")))
