b39582f60af8d34e8d16762cfc9d80699e0469d5
Dennis Riis <driis@ebay.com>
2013-08-27 22:43:55 +0200

    Working dna.clj

--- clojure/nucleotide-count/dna.clj
+++ clojure/nucleotide-count/dna.clj
@@ -1,7 +1,13 @@
 (ns dna)
 
-(defn count [strand dna-string]
-  0)
-
 (defn nucleotide-counts [dna-string]
-  {})
+  (let [empty {\A 0, \T 0, \C 0, \G 0}]
+    (merge-with + empty (frequencies dna-string))))
+
+(defn count [strand dna-string]
+  (let [allowed-nucleotide [\A \T \C \G \U]]
+    (if (not (some #{strand} allowed-nucleotide))
+       (throw (Exception. "invalid nucleotide"))))
+
+  (let [dna (nucleotide-counts dna-string)]
+    (get (nucleotide-counts dna-string) strand 0)))
