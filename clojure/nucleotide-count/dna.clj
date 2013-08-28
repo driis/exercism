e622f8a069bb5950176f3241e7af095ad78dcb83
Dennis Riis <driis@ebay.com>
2013-08-28 22:49:02 +0200

    Assert in separate function

--- clojure/nucleotide-count/dna.clj
+++ clojure/nucleotide-count/dna.clj
@@ -1,13 +1,15 @@
 (ns dna)
 
-(defn nucleotide-counts [dna-string]
+(defn- assert-allowed-strand [strand]
+  (let [allowed-nucleotide #{\A \T \C \G \U}]
+    (when-not (allowed-nucleotide strand)
+      (throw (Exception. "invalid nucleotide")))))
+
+(defn nucleotide-counts [dna-stri ng]
   (let [empty {\A 0, \T 0, \C 0, \G 0}]
     (merge empty (frequencies dna-string))))
 
 (defn count [strand dna-string]
-  (let [allowed-nucleotide #{\A \T \C \G \U}]
-    (when (not (allowed-nucleotide strand))
-       (throw (Exception. "invalid nucleotide"))))
-
+  (assert-allowed-strand strand)
   (let [dna (nucleotide-counts dna-string)]
     (get (nucleotide-counts dna-string) strand 0)))
