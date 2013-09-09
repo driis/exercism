c400998dd39ef3510491a2103d1999ef4fd047cf
Dennis Riis <driis@ebay.com>
2013-09-09 21:42:14 +0200

    Add suggestions from exercism, and get rid of unused methods.

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,21 +1,17 @@
 class Phrase 
     def initialize words
-        @words_map = map_words words
+        @words = words
     end
 
     def word_count
-        return @words_map
+        map_words @words
     end
 
     private
-    def sum_word(acc, word)
-        acc[word] = (acc[word] || 0) + 1
-    end
-
     def map_words words
         word_list = words.downcase.scan /\w+/
-        word_list.reduce(Hash.new) {|acc,word| 
-            acc[word] = (acc[word] || 0) + 1
+        word_list.reduce(Hash.new(0)) {|acc,word|
+            acc[word] = acc[word] + 1
             acc
         }
     end    
