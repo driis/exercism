c65a049528409a3807f705737c7876140d13f270
Dennis Riis <driis@ebay.com>
2013-09-09 22:11:41 +0200

    Word count in Ruby; style changes and getting rid of extra cruft.

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,18 +1,17 @@
-class Phrase 
-    def initialize words
-        @words = words
-    end
+class Phrase
+  def initialize words
+    @words = words
+  end
 
-    def word_count
-        map_words @words
+  def word_count
+    word_list = get_word_list(@words)
+    word_list.each_with_object(Hash.new(0)) do |word, word_counts|
+      word_counts[word] = word_counts[word] + 1
     end
+  end
 
-    private
-    def map_words words
-        word_list = words.downcase.scan /\w+/
-        word_list.reduce(Hash.new(0)) {|acc,word|
-            acc[word] = acc[word] + 1
-            acc
-        }
-    end    
+  private
+  def get_word_list(words)
+    words.downcase.scan /\w+/
+  end
 end
