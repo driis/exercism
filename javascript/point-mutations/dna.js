c1cb58d3f530939ee9def21cf5efc5884d797597
Dennis Riis <driis@ebay.com>
2013-09-09 21:58:13 +0200

    Implement hamming distance in JavaScript

---
+++ javascript/point-mutations/dna.js
@@ -0,0 +1,19 @@
+var DNA = function(dna) {
+    "use strict";
+    dna = dna.split("");
+    
+    return {
+        hammingDistance: function(otherDna) {
+            otherDna = otherDna.split("");
+            var longest = dna.length > otherDna.length ? dna : otherDna;
+            var shortest = dna.length > otherDna.length ? otherDna : dna;
+
+            return shortest.reduce(function(acc, val, index) {
+                if (val !== longest[index])
+                    acc++;
+                return acc;
+            }, 0);
+        }
+    };
+};
+module.exports = DNA;
