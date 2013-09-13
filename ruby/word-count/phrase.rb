d5b01f17a8f1abe138ae119db9e39069124f3769
Dennis Riis <driis@ebay.com>
2013-09-13 20:29:15 +0200

    Last change to the ruby word_count solution

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -4,14 +4,13 @@ class Phrase
   end
 
   def word_count
-    word_list = get_word_list
     word_list.each_with_object(Hash.new(0)) do |word, counts|
       counts[word] = counts[word] + 1
     end
   end
 
   private
-  def get_word_list
+  def word_list
     @words.downcase.scan /\w+/
   end
 end
