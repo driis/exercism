fc08875c419b3ec26e2a794e1a0a4e53c13eb5c5
Dennis Riis <driis@ebay.com>
2013-10-24 20:14:28 +0200

    Use deconstruction in parameters and save the let.

--- clojure/etl/etl.clj
+++ clojure/etl/etl.clj
@@ -1,9 +1,8 @@
 (ns etl 
   (:require [clojure.string :as str]))
 
-(defn- pairs [pair]
-  (let [[value key] pair]
-    (map (fn [k] {(str/lower-case k) value}) key)))
+(defn- pairs [[value key]]
+    (map (fn [k] {(str/lower-case k) value}) key))
     
 (defn transform [words]
   (into {} (flatten (map pairs words))))
